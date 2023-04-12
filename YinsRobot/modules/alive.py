051c86ff9de3d4c5e357f3dbe568889ed656ad37
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-04-12 13:21:25 +0700

    Update alive.py

--- YinsRobot/modules/alive.py
+++ YinsRobot/modules/alive.py
@@ -9,17 +9,17 @@ from YinsRobot.events import register
 from YinsRobot import telethn as tbot
 
 yinzver = "2.0.22"
-PHOTO = "https://telegra.ph/file/8862c270425cb949cfba0.jpg"
+PHOTO = "https://telegra.ph/file/490835371cdde6058e740.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Ragna Robot.** \n\n"
-  TEXT += "✨ **I'm Working Properly** \n\n"
-  TEXT += f"✨ **Oᴡɴᴇʀ : [ᴢᴇʏ](https://t.me/Archanistz)** \n\n"
-  TEXT += f"✨ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ    :** `{telever}` \n\n"
-  TEXT += f"✨ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ   :** `{tlhver}` \n\n"
-  TEXT += f"✨ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += f"✨ **RᴀɢɴᴀXRᴏʙᴏᴛ Vᴇʀsɪᴏɴ :** `{yinzver}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ✨**"
-  BUTTON = [[Button.url("Help", "https://t.me/Zenv1bot?start=help"), Button.url("Support", "https://t.me/Zenzproject")]]
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm GSID Robot.** \n\n"
+  TEXT += "✨ **Saya bekerja dengan benar** \n\n"
+  TEXT += f"✨ **Owner : [Idamanmu](https://t.me/Archanistz)** \n\n"
+  TEXT += f"✨ **Library Version    :** `{telever}` \n\n"
+  TEXT += f"✨ **Telethon Version   :** `{tlhver}` \n\n"
+  TEXT += f"✨ **Pyrogram Version   :** `{pyrover}` \n\n"
+  TEXT += f"✨ **GSIDRobot Version  :** `{yinzver}` \n\n"
+  TEXT += "**Terima Kasih Telah Menambahkan Saya ✨**"
+  BUTTON = [[Button.url("Bantuan", "https://t.me/goldensid_bot?start=help"), Button.url("Support", "https://t.me/Zenzproject")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
