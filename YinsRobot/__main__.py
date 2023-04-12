cce97c6bbcc1927097d9ef442e86e98a6f51f74c
Ayub <126319011+Ayubskk@users.noreply.github.com>
2023-04-12 16:35:51 +0700

    Update __main__.py

--- YinsRobot/__main__.py
+++ YinsRobot/__main__.py
@@ -111,7 +111,7 @@ buttons = [
 
 
 HELP_STRINGS = """
-Click on the button bellow to get description about specifics command."""
+Klik tombol di bawah untuk mendapatkan informasi tentang perintah."""
 
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
